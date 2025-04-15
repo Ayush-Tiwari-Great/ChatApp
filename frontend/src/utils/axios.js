6ae108a54d69834e0a73ddac7a161009ffcca5e2
Ayush <ayushtiwari7891>
2025-04-15 21:01:29 +0530

    change origin

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:2000": "/",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:2000": import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
