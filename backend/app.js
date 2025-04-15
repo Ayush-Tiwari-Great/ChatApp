8f6c4d56cd1d76313ac180c467b4cfbd1948b3dc
Ayush <ayushtiwari7891>
2025-04-15 20:23:04 +0530

    change origin

--- backend/app.js
+++ backend/app.js
@@ -85,12 +85,6 @@ app.use((req, res) => {
   });
   
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-    })
-}
   
 server.listen(2000, ()=>{
     console.log("lestning");
